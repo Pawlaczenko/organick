01fcb4c0b3b3a0da7867002f85f2f6a1b4ec1a0a
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2023-04-27 17:40:29 +0200

    Add Offers Section. Add OfferWindow component. Apply RWD

---
+++ src/components/Sections/OffersSection/OffersSection.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import Section from '../../../layout/Section';
+import OfferWindow from '../../OfferWindow/OfferWindow';
+import Heading, { HeadingLevel } from '../../Heading/Heading';
+import offer_1 from '../../../assets/images/offer-1.jpg';
+import offer_2 from '../../../assets/images/offer-2.jpg';
+import { BREAKPOINTS } from '../../../styles/variables';
+
+const OffersSection : FC = () => {
+    return (
+        <OffersSectionWrapper>
+            <StyledOffersSection>
+                <OfferWindow backgroundImage={offer_1} text='Get Garden Fresh Fruits' upperText='Natural!!' variant='light' />
+                <OfferWindow backgroundImage={offer_2} text='Get 10% off on Vegetables' upperText='Offer!!' variant='dark' />
+            </StyledOffersSection>
+        </OffersSectionWrapper>
+    )
+}
+
+const OffersSectionWrapper = styled(Section)`
+    margin: 15rem 0;
+
+    @media only screen and (${BREAKPOINTS.burger}){
+        margin: 8rem 0;
+    }
+`;
+
+
+const StyledOffersSection = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(min(90%,68rem),1fr));
+    gap: 3.6rem;
+`;
+
+
+export default OffersSection
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import Page from '../layout/Page'
-import HeroSection from '../components/HeroSection/HeroSection'
+import HeroSection from '../components/Sections/HeroSection/HeroSection'
+import OffersSection from '../components/Sections/OffersSection/OffersSection'
 
 const HomePage : FC = () => {
   return (
     <Page>
       <HeroSection />
+      <OffersSection />
     </Page>
   )
 }
